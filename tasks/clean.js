6d4b683d9ac1835ef8041b537c324aab78d301ae
Bruno Wego <brunowego@gmail.com>
2016-07-23 16:57:31 -0300

    chore(grunt): added dist task

--- tasks/clean.js
+++ tasks/clean.js
@@ -1,3 +1,4 @@
 module.exports = {
+  dist: './public/assets/*',
   server: './tmp/*'
 };
--- tasks/sass.js
+++ tasks/sass.js
@@ -2,6 +2,13 @@ module.exports = {
   options: {
     includePaths: ['./node_modules']
   },
+  dist: {
+    expand: true,
+    cwd: './src/styles',
+    src: '*.scss',
+    dest: './public/assets/styles',
+    ext: '.css'
+  },
   server: {
     expand: true,
     cwd: './src/styles',
