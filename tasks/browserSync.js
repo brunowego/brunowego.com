e5ed1b585d7e3ff686be77a47ffe25e0f479b559
Bruno Wego <brunowego@gmail.com>
2016-07-23 17:11:02 -0300

    chore(page): added footer

--- tasks/browserSync.js
+++ tasks/browserSync.js
@@ -9,7 +9,7 @@ module.exports = {
         './tmp/assets/styles/*.css'
       ],
       server: {
-        baseDir: ['./public', './tmp']
+        baseDir: ['./tmp', './public']
       }
     }
   }
