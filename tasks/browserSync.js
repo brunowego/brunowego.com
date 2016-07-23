efe6e5cee7cf801d66956ed5a532dc051c17d07f
Bruno Wego <brunowego@gmail.com>
2016-07-23 16:49:28 -0300

    chore(grunt): added watch support

--- tasks/browserSync.js
+++ tasks/browserSync.js
@@ -1,6 +1,13 @@
 module.exports = {
+  options: {
+    background: true
+  },
   server: {
     options: {
+      files: [
+        './public/*.html',
+        './tmp/assets/styles/*.css'
+      ],
       server: {
         baseDir: ['./public', './tmp']
       }
